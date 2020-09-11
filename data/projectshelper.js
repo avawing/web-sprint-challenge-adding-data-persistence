60db7f849fe9bee96460096f99135e2fc4e18943
Aveline Wingfield <avelinewingfield@gmail.com>
2020-09-11 12:24:42 -0400

    Project insert workaround

--- data/projectshelper.js
+++ data/projectshelper.js
@@ -6,13 +6,19 @@ function find(){
 }
 
 //get project by id
+function findById(id){
+    return db('projects').where({id}).first()
+}
 
 //get tasks by project id
 
 //get resources (resource helper)
 
 //post project
+function add(data) {
+    return db('projects').insert(data)
+  }
 //post task to project by id
 //post resource (resource helper)
 
-module.exports = {find}
+module.exports = {find, findById, add}
--- routes/projectRoute.js
+++ routes/projectRoute.js
@@ -1,32 +1,60 @@
 const express = require("express");
-const db = require('../data/projectshelper')
+const db = require("../data/projectshelper");
 
 const projectRoute = express.Router();
 
-projectRoute.get('/', (req, res)=>{
-    db.find()
-    .then(projects =>{
-        if(projects.length){
-            res.status(200).json(projects).end()
-        }else{
-            res.status(404).json({message: "There are no projects on the server"}).end()
-        }
+projectRoute.get("/", (req, res) => {
+  db.find()
+    .then((projects) => {
+      if (projects.length) {
+        res.status(200).json(projects).end();
+      } else {
+        res
+          .status(404)
+          .json({ message: "There are no projects on the server" })
+          .end();
+      }
     })
-    .catch(err => {
-        res.status(500).json({message: "The server exploded"})
+    .catch((err) => {
+      res.status(500).json({ message: "The server exploded" }).end();
+    });
+});
+
+projectRoute.get("/:id", (req, res) => {
+  db.findById(req.params.id)
+    .then((project) => {
+      if (project) {
+        res.status(200).json(project).end();
+      } else {
+        res.status(404).json({ message: "Project not found" }).end();
+      }
     })
-})
+    .catch((err) => {
+      res.status(500).json({ message: "The server exploded" }).end();
+    });
+});
 
-projectRoute.get('/:id', (req, res)=>{
-    res.send('This is a project')
-})
+projectRoute.get("/:id/tasks", (req, res) => {
+  res.send("Project Tasks");
+});
 
-projectRoute.get('/:id/tasks', (req, res)=>{
-    res.send('Project Tasks')
-})
-
-projectRoute.post('/', (req, res)=>{
-    res.send('Posted')
-})
+projectRoute.post("/", (req, res) => {
+  db.add(req.body)
+    .then((project) => {
+      if (project) {
+        db.findById(project[0])
+          .then((project) => res.status(201).json(project).end())
+          .catch((err) => res.status(500).json({ message: "bigger error" }));
+      } else {
+        res
+          .status(400)
+          .json({ message: "Bad Request - please fill all required fields" })
+          .end();
+      }
+    })
+    .catch((err) => {
+      res.status(500).json(err).end();
+    });
+});
 
 module.exports = projectRoute;
